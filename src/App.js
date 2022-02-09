c4e26e0c822ee6ca3052a827412f06899d9863f8
Svetlana Prus <swetlana.prus@gmail.com>
2022-02-09 04:27:15 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import factory from './images/factory 1.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <section className="header_items">
+          <p>ALECTIA</p>
+          <div>
+            <p><a href="#top">Home</a></p>
+            <p><a href="#top">Services</a></p>
+            <p><a href="#top">Projects</a></p>
+            <p><a href="#top">Careers</a></p>
+            <p><a href="#top">About ALECTIA</a></p>
+          </div>
+        </section>
       </header>
+      <main className="main">
+        <section className="gallery">
+          <div className="gallery_items">
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+
+            <div className="gallery_item_vert">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item_big">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+            <div className="gallery_item">
+              <img src={factory} alt="factory" />
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <div>*** FOOTER ***</div>
+      </footer>
     </div>
   );
 }
